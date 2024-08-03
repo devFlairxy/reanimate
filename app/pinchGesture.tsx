9d83436809e692bff138901dc7794e0282b8ee23
Poseidon <122105735+poseidonxy@users.noreply.github.com>
2024-08-03 21:57:22 +0100

    focal point

--- app/pinchGesture.tsx
+++ app/pinchGesture.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import React from 'react';
 import {
   Gesture,
@@ -6,22 +6,79 @@ import {
   GestureHandlerRootView,
   PinchGestureHandler,
 } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 const imageUrl =
-  'https://plus.unsplash.com/premium_photo-1722009043668-b3c470595df9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D';
+  'https://images.unsplash.com/photo-1721332155433-3a4b5446bcd9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D';
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+const { width, height } = Dimensions.get('window');
 
 const PinchGesture = () => {
+  const focalX = useSharedValue(0);
+  const focalY = useSharedValue(0);
+  const scale = useSharedValue(1);
   const pinch = Gesture.Pinch()
-    .onStart(() => {})
-    .onUpdate(() => {})
+    .onUpdate((event) => {
+      focalX.value = event.focalX;
+      focalY.value = event.focalY;
+      scale.value = event.scale;
+    })
+    .onEnd(() => {
+      scale.value = withTiming(1)
+    })
     .runOnJS(true);
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        //transform is executed in the arrangement order
+        { translateX: focalX.value },
+        { translateY: focalY.value },
+        { translateX: -width / 2 },
+        { translateY: -height / 2 },
+        {scale: scale.value},
+        //reverting back to original after scaling by multiplying by -1
+        { translateX: -focalX.value },
+        { translateY: -focalY.value },
+        { translateX: width / 2 },
+        { translateY: height / 2 },
+      ],
+    };
+  });
+
+  const rFocusStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: focalX.value }, { translateY: focalY.value }],
+    };
+  });
   return (
     <GestureHandlerRootView>
       <GestureDetector gesture={pinch}>
-        <Image style={{ flex: 1 }} source={{ uri: imageUrl }} />
+        <Animated.View style={{ flex: 1 }}>
+          <AnimatedImage
+            style={[{ flex: 1 }, rStyle]}
+            source={{ uri: imageUrl }}
+          />
+          <Animated.View style={[styles.focalPoint, rFocusStyle]} />
+        </Animated.View>
       </GestureDetector>
     </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  focalPoint: {
+    ...StyleSheet.absoluteFillObject,
+    width: 20,
+    height: 20,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+  },
+});
 
 export default PinchGesture;
